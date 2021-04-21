6d409b992af929e3fbf7cba681e80b78447301e8
magfi14 <kupershe@kean.edu>
2021-04-21 15:15:33 -0400

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,14 +1,29 @@
 public class Movie {
 
+    /**
+     * Instance Fields
+     */
+
     private int percentageIdentificationNumber;
     private int numberSeen;
     private float averageRating;
     private String movieName;
     private int releaseYear;
 
+    /* Constructor */
+
     public Movie() {
 
     }
+
+    /**
+     * Creates a new Movie object
+     * @param percentageId The distribution of the percentage of people who gave a rating
+     * @param numSeen The number of people who saw the movie
+     * @param avgRate The average rating given by people who saw the movie
+     * @param name The name of the movie
+     * @param relYear The release year
+     */
 
     public Movie(int percentageId, int numSeen, float avgRate, String name, int relYear) {
 
@@ -21,11 +36,21 @@ public class Movie {
 
     }
 
+    /**
+     *
+     * @return The distribution of the percentage of people who gave a rating
+     */
+
     public int getPercentID() {
 
         return this.percentageIdentificationNumber;
 
     }
+
+    /**
+     * Input the percentage of distribution of people who assigned a rating
+     * @param percentID The distribution of the percentage of people who gave a rating
+     */
 
     public void setPercentID(int percentID) {
 
@@ -33,11 +58,21 @@ public class Movie {
 
     }
 
+    /**
+     *
+     * @return The number of people who saw the movie
+     */
+
     public int getNumberSeen() {
 
         return this.numberSeen;
 
     }
+
+    /**
+     *
+     * @param numberSeen The number of people who saw the movie
+     */
 
     public void setNumberSeen(int numberSeen) {
 
@@ -45,11 +80,21 @@ public class Movie {
 
     }
 
+    /**
+     *
+     * @return The average rating assigned by viewers who watched the movie
+     */
+
     public float getAvgRating() {
 
         return this.averageRating;
 
     }
+
+    /**
+     *
+     * @param avgRate The average rating assigned by viewers who watched the movie
+     */
 
     public void setAverageRating(float avgRate) {
 
@@ -57,11 +102,21 @@ public class Movie {
 
     }
 
+    /**
+     *
+     * @return The name of the movie
+     */
+
     public String getMovieName() {
 
         return this.movieName;
 
     }
+
+    /**
+     *
+     * @param movieName The name of the movie
+     */
 
     public void setMoviename(String movieName) {
 
@@ -69,17 +124,32 @@ public class Movie {
         
     }
 
+    /**
+     *
+     * @return The year the movie was released
+     */
+
     public int getRelYear() {
 
         return this.releaseYear;
 
     }
 
+    /**
+     *
+     * @param releaseYear The year the movie was released
+     */
+
     public void setRelYear(int releaseYear) {
 
         this.releaseYear = releaseYear;
 
     }
+
+    /**
+     *
+     * @return A properly formatted string depiction of the movie details
+     */
 
     public String toString() {
 
