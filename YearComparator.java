7765c75d92ae25e7893b66c98585789dd6932f17
Healiam <83127643+Healiam@users.noreply.github.com>
2021-04-24 18:21:58 -0400

    Update YearComparator.java

--- YearComparator.java
+++ YearComparator.java
@@ -1,33 +1,14 @@
-//Author: Liam Healy
-
 import java.util.Comparator;
 
-public class YearComparator implements Comparator {
+public class YearComparator implements Comparator<Movie> {
 
-    //Instance Variable
-    private int releaseYear;
-
-    public YearComparator(int year) {
-
-        this.releaseYear = year;
-
-    }
-
-    public int getYear() {
-
-        return this.releaseYear;
-
-    }
-
-
-    @Override
-    public int compare(Object o1, Object o2) {
-
-        YearComparator comparator1 = (YearComparator) o1;
-        YearComparator comparator2 = (YearComparator) o2;
-
-        return comparator2.getYear() - comparator1.getYear();
-
+    public int compare(Movie m1, Movie m2) {
+        if (m1.getRelYear() < m2.getRelYear())
+            return 1;
+        else if (m1.getRelYear() > m2.getRelYear())
+            return -1;
+        else
+            return 0;
     }
 
 }
