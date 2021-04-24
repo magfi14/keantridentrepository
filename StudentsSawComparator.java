fbec6d6815b7756af4dae3e2ef1cf59fc8ff4981
Healiam <83127643+Healiam@users.noreply.github.com>
2021-04-24 18:22:04 -0400

    Update StudentsSawComparator.java

--- StudentsSawComparator.java
+++ StudentsSawComparator.java
@@ -1,52 +1,14 @@
-/**
- * @author Ethan Kupershtein
- */
-
 import java.util.Comparator;
 
-public class StudentsSawComparator implements Comparator {
+public class StudentsSawComparator implements Comparator<Movie> {
 
-    /** Instance Variables */
-
-    private int numberOfStudents;
-
-    /**
-     *
-     * @param numberOfStudents The number of students who watched the movie
-     */
-
-    public StudentsSawComparator(int numberOfStudents) {
-
-        this.numberOfStudents = numberOfStudents;
-
-    }
-
-    /**
-     *
-     * @return The comparable number of students who watched the movie
-     */
-
-    public int getNumberOfStudents() {
-
-        return this.numberOfStudents;
-
-    }
-
-    /**
-     *
-     * @param o1 The first comparator
-     * @param o2 The second comparator
-     * @return The difference between the 2 movies
-     */
-
-    @Override
-    public int compare(Object o1, Object o2) {
-
-        StudentsSawComparator comparator1 = (StudentsSawComparator) o1;
-        StudentsSawComparator comparator2 = (StudentsSawComparator) o2;
-
-        return comparator2.getNumberOfStudents() - comparator1.getNumberOfStudents();
-
+    public int compare(Movie m1, Movie m2) {
+        if (m1.getNumberSeen() < m2.getNumberSeen())
+            return 1;
+        else if (m1.getNumberSeen() > m2.getNumberSeen())
+            return -1;
+        else
+            return 0;
     }
 
 }
