2fe89ece92b56b3dbbd106a5677bb66f16b33e35
Healiam <83127643+Healiam@users.noreply.github.com>
2021-04-24 11:56:05 -0400

    Update RatingComparator.java

--- RatingComparator.java
+++ RatingComparator.java
@@ -1,5 +1,29 @@
 //Author: Liam Healy
 
-public class RatingComparator {
+import java.util.Comparator;
+
+public class RatingComparator implements Comparator {
+
+    //Instance Variable
+    private float averageRating;
+    
+    public RatingComparator(float rating) {
+
+        this.averageRating = rating;
+    }
+    
+    public float getComparableRating() {
+
+        return this.averageRating;
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        
+        RatingComparator compare1 = (RatingComparator) o1;
+        RatingComparator compare2 = (RatingComparator) o2;
+
+        return (int) (compare2.getComparableRating() - compare1.getComparableRating());
+    }
     
 }
