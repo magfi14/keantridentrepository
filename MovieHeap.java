e878ea36276ccc535e3dc94cb305ddb9ab22662d
magfi14 <kupershe@kean.edu>
2021-04-24 13:04:02 -0400

    Committed

--- MovieHeap.java
+++ MovieHeap.java
@@ -2,24 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class MovieHeap {
-    
+
     public static void main(String[] args)throws IOException {
-    //Create Priority Queue Heap
-    PriorityQueue<Integer> pQueue_heap = new PriorityQueue<Integer>(Collections.reverseOrder());
+        //Create Priority Queue Heap
+        PriorityQueue<Integer> pQueue_heap = new PriorityQueue<Integer>(Collections.reverseOrder());
 
-    //Create File and Scanner
-    File movies = new File("movies.txt");
-    Scanner scan = new Scanner(movies);
+        //Create File and Scanner
+        File movies = new File("movies.txt");
+        Scanner scan = new Scanner(movies);
 
-    //Loop to read File
-    while (scan.hasNextLine()) {
-        int ID = scan.nextInt();
-        int seen = scan.nextInt();
-        int rating = scan.nextInt();
-        String title = scan.next();
-        int year = scan.nextInt();
+        //Loop to read File
+        while (scan.hasNextLine()) {
+            int ID = scan.nextInt();
+            int seen = scan.nextInt();
+            float rating = scan.nextFloat();
+            String title = scan.next();
+            int year = scan.nextInt();
+        }
+
+        //Close Scanner
+        scan.close();
     }
 
-    }
 }
-
