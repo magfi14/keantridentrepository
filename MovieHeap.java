f57ea463a99f05cc88d8bcd308d6278f071e17ae
Healiam <83127643+Healiam@users.noreply.github.com>
2021-04-24 12:34:26 -0400

    Update MovieHeap.java

--- MovieHeap.java
+++ MovieHeap.java
@@ -1,3 +1,11 @@
+import java.io.File;
+import java.util.*;
+
 public class MovieHeap {
     
+    PriorityQueue<Integer> pQueue_heap = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+    File movies = new File();
+	Scanner scan = new Scanner(movies);
+
 }
