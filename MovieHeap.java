f4cccecde379c51b5f1059718c1aa3883129adff
Healiam <83127643+Healiam@users.noreply.github.com>
2021-04-24 12:45:53 -0400

    Update MovieHeap.java

--- MovieHeap.java
+++ MovieHeap.java
@@ -1,11 +1,21 @@
-import java.io.File;
+import java.io.*;
 import java.util.*;
 
 public class MovieHeap {
     
+    public static void main(String[] args)throws IOException {
+    //Create Priority Queue Heap
     PriorityQueue<Integer> pQueue_heap = new PriorityQueue<Integer>(Collections.reverseOrder());
 
-    File movies = new File();
-	Scanner scan = new Scanner(movies);
+    //Create File and Scanner
+    File movies = new File("movies.txt");
+    Scanner scan = new Scanner(movies);
 
+    //Loop to read File
+    while (scan.hasNextLine()) {
+        
+    }
+
+    }
 }
+
