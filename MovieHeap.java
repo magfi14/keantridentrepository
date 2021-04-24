77a62ec6b8b28997439cf7e7b2cd71e42a3c30c9
magfi14 <kupershe@kean.edu>
2021-04-24 12:55:05 -0400

    Committed

--- MovieHeap.java
+++ MovieHeap.java
@@ -1,25 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class MovieHeap {
-    
-    public static void main(String[] args)throws IOException {
-    //Create Priority Queue Heap
-    PriorityQueue<Integer> pQueue_heap = new PriorityQueue<Integer>(Collections.reverseOrder());
 
-    //Create File and Scanner
-    File movies = new File("movies.txt");
-    Scanner scan = new Scanner(movies);
+    public static void main(String[] args) throws IOException {
+        //Create Priority Queue Heap
+        PriorityQueue<Integer> pQueue_heap = new PriorityQueue<Integer>(Collections.reverseOrder());
 
-    //Loop to read File
-    while (scan.hasNextLine()) {
-        int ID = scan.nextInt();
-        int seen = scan.nextInt();
-        int rating = scan.nextInt();
-        String title = scan.next();
-        int year = scan.nextInt();
-    }
+        //Create File and Scanner
+        File movies = new File("movies.txt");
+        Scanner scan = new Scanner(movies);
+
+        //Loop to read File
+        while (scan.hasNextLine()) {
+            int ID = scan.nextInt();
+            int seen = scan.nextInt();
+            int rating = scan.nextInt();
+            String title = scan.next();
+            int year = scan.nextInt();
+        }
 
     }
 }
-
