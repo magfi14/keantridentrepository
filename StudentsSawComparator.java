3f9c232eb52d22f09c1533dfcc5349306bd5b898
magfi14 <kupershe@kean.edu>
2021-04-24 11:48:20 -0400

    Committed

--- StudentsSawComparator.java
+++ StudentsSawComparator.java
@@ -1,5 +1,48 @@
-public class StudentsSawComparator {
+import java.util.Comparator;
 
+public class StudentsSawComparator implements Comparator {
 
+    /** Instance Variables */
+
+    private int numberOfStudents;
+
+    /**
+     *
+     * @param numberOfStudents The number of students who watched the movie
+     */
+
+    public StudentsSawComparator(int numberOfStudents) {
+
+        this.numberOfStudents = numberOfStudents;
+
+    }
+
+    /**
+     *
+     * @return The comparable number of students who watched the movie
+     */
+
+    public int getNumberOfStudents() {
+
+        return this.numberOfStudents;
+
+    }
+
+    /**
+     *
+     * @param o1 The first comparator
+     * @param o2 The second comparator
+     * @return The difference between the 2 movies
+     */
+
+    @Override
+    public int compare(Object o1, Object o2) {
+
+        StudentsSawComparator comparator1 = (StudentsSawComparator) o1;
+        StudentsSawComparator comparator2 = (StudentsSawComparator) o2;
+
+        return comparator2.getNumberOfStudents() - comparator1.getNumberOfStudents();
+
+    }
 
 }
