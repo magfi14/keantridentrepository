c5168819dcc284b97747bcfe600ea18c2689ac64
Healiam <83127643+Healiam@users.noreply.github.com>
2021-05-03 08:35:57 -0400

    Update RatingComparator.java

--- RatingComparator.java
+++ RatingComparator.java
@@ -1,3 +1,7 @@
+//Authors: Ethan Kupershtein, Liam Healy, Davian Brown, Daniel Gneco
+//05/04/2021
+//RatingComparator- This is the comparator for the rating 
+
 import java.util.Comparator;
 
 public class RatingComparator implements Comparator<Movie> {
